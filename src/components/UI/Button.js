2d9cf65e2bdc948c0cd7045d9d9979faae2af1de
Ali Karabulut <a.kbt6666@gmail.com>
2022-11-16 01:58:18 +0300

    Button component start

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,27 @@
 
-const Button = props =>{
-    return()
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import LogoutIcon from "@mui/icons-material/Logout";
 
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import PersonIcon from "@mui/icons-material/Person";
+
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import styles from './Button.module.css'
+
+
+const Button = props => {
+
+    // const MenuButton = (icon,name) =>{
+    // }
+
+    return(
+        <button className={styles["menu-button"]}>
+        <span className={styles["button-icon"]}>{props.icon}</span>
+        <span className={styles["button-name"]}>{props.name}</span>
+    </button>
+    )
 }
 
 export default Button
